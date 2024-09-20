88c509165f421ecd11706de9b1b03cc28378a89f
ka8540 <ka8540@g.rit.edu>
2024-09-20 08:02:48 -0400

    edit pom file

--- Assignment1/src/test/java/com/assignment/DemoApplicationTests.java
+++ Assignment1/src/test/java/com/assignment/DemoApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Assignment1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
